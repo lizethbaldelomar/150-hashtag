7897c401b799440d56302aff7c23a8e1402625db
Justine <justinekapunan@gmail.com>
2016-06-01 10:25:45 +0800

    Final parser + Written report

--- translated.py
+++ translated.py
@@ -1,15 +1,7 @@
 def main():
-	print "Enter n "
-	n = input()
-	fib(n)
-
-def fib(n):
-	a=0
+	a=1.0
 	b=1
-	for i in range (0, n):
-		temp = a
-		a = b
-		b = temp+b
-	print a
+	n = a+b
+	print n
 
 main()
